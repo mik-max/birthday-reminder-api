49a4916a2a1adfd9240c9dc709bcf4217d243ba7
Michael Chinye <michaelchinye2018@gmail.com>
2022-09-02 23:00:32 +0100

    Added swagger documentation of all endpoints of the api

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import Cors from 'cors';
 import schedule from 'node-schedule'
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' assert {type: "json"};
 
 //Routers
 import churchesRouter from './src/routes/churchesRoutes.js';
@@ -21,6 +23,12 @@ app.use('/api', churchesRouter)
 app.use('/api', membersRouter)
 app.use('/api', usersRouter)
 app.use('/api', emailRouter)
+
+// Swagger Documentation 
+var options = {
+     explorer: true,
+};
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 schedule.scheduleJob('00 1 00 * * *', async function() {
      // This will run every Day at 12:00;
